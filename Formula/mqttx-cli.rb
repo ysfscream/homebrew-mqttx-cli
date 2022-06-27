7d528e1ceafe74f7ff7791b081da323df1cb5e96
ysfscream <894402575bt@gmail.com>
2022-06-27 10:00:16 +0800

    test

--- Formula/mqttx-cli.rb
+++ Formula/mqttx-cli.rb
@@ -4,7 +4,7 @@ class MqttxCli < Formula
   desc "MQTT X CLI is an open source MQTT 5.0 command line client and MQTT X on the command line."
   homepage "https://github.com/emqx/MQTTX/tree/main/cli"
   url "https://github.com/emqx/MQTTX/releases/download/v1.8.0/mqttx-cli.tar.gz"
-  sha256 "f40883277f9fff8c916f1aeee169d3a647af3c66a214996e0fac17060451f153"
+  sha256 "d9b8db76bb966fa5e9fcdc3b1fc3a17e3302c5dc9cb6f05e20eead814b37241a"
   license "Apache"
   version "1.8.0"
 
