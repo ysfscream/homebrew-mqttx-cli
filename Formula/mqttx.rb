fb8c49ea8a9ca5c97d26835c0f83550d7a6beb06
ysfscream <894402575bt@gmail.com>
2022-06-27 09:49:37 +0800

    test

---
+++ Formula/mqttx.rb
@@ -0,0 +1,16 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+class Mqttx < Formula
+  desc "MQTT X CLI is an open source MQTT 5.0 command line client and MQTT X on the command line."
+  homepage "https://github.com/emqx/MQTTX/tree/main/cli"
+  url "https://github.com/emqx/MQTTX/releases/download/v1.8.0/mqttx-cli.tar.gz"
+  sha256 "f40883277f9fff8c916f1aeee169d3a647af3c66a214996e0fac17060451f153"
+  license "Apache"
+  version "1.8.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install "mqttx"
+  end
+end
---
+++ README.md
@@ -0,0 +1,7 @@
+# homebrew-mqttx
+
+```shell
+brew tap emqx/mqttx
+
+brew install mqttx
+```
